f9d13403606ce2316636af93cfd1f2880e00b66a
Gavin Allen <goonerlagoon@gmail.com>
2021-12-08 01:08:01 -0500

    first fully working version

--- tubegrabber.py
+++ tubegrabber.py
@@ -1,104 +1,68 @@
 import json
 from yt_dlp import YoutubeDL
 from pathlib import Path
-# import tkinter as tk
+import tkinter as tk
 from tkinter import filedialog as fd
 
-# root = tk.Tk()
-# root.title("Greetings Human")
+root = tk.Tk()
+root.title("Greetings Human")
+root.withdraw()
 
+def sep_sanitize(file_str):
+    try:
+        return str(Path(file_str))
+    except TypeError:
+        print("wrong type of object passed)
+    
 
-# dict to load all process vars like 'dl_folder'
+# dict to load all vars like 'dl_folder' etc from json state file 
 data = {}
+data_file = 'ydl_data.json'
+  
+data['url_to_download'] = "https://www.youtube.com/playlist?list=PLzfMfiJ36mokTWgmaZgOdq3utSSbEaAIf"
 
 try:
-    with open('ydl_data.txt', 'r') as f:
-        data = json.loads(f) # dump process vars into program
+    with open(data_file, 'r') as f:
+        data = json.load(f) # dump process vars into program
 except FileNotFoundError:
-    print("First time running the script? Let's get you set up then.")
-    chosen_dir = fd.askdirectory(title="Select Default Download Destination")
-    dl_fldr = Path(chosen_dir)
-    data['dl_folder'] = dl_fldr.resolve()
-    data['archive_file'] = (dl_fldr / 'ydl_archive.txt').touch(exist_ok=False)
+    print("First time running the script it seems. Let's set up shop then.")
+    # casting resulting file string to Path object to get proper seperator
+    chosen_dir = Path(fd.askdirectory(title="Select Default Download Destination")) 
     
-    chosen_file = ''
-    while chosen_file.endswith('.txt') != True:        
-        chosen_file = fd.askopenfilename(title="Select Archive File. If it does"
-                                        "not exist, choose a directory"
-                                        "to make it in. Hit Cancel to"
-                                        "choose the current working"
-                                        "directory")
-        if Path(chosen_file).is_file():
-            if Path(chosen_file).suffix == '.txt':
-                data['archive_file'] = chosen_file
-                break
-            else:
-                continue
-        else: #has to be a directory if not a file
-                a_file = Path(chosen_file / 'archive_file.txt')
-                a_file.touch(exist_ok=False)
-                data['archive_file'] = a_file.resolve()
-               
-    
-        
-    dl_fldr = Path(data['dl_folder'] / 'ydl_data.txt')
-    dl_fldr.touch(exist_ok=False)
-    
+    data['dl_folder'] = str(chosen_dir)
 except PermissionError:
     print("Insufficient permission to read that file")
-    
 
-    
-url_to_download = "https://www.youtube.com/playlist?list=PLzfMfiJ36mokTWgmaZgOdq3utSSbEaAIf"
+root.destroy()
+root.mainloop()
 
-ydl_opts = { 
+# make archive_text file to hold already dl'd vids
+archive_file = (Path(data['dl_folder']) / 'ydl_archive.txt')
+archive_file.touch()
+data['archive_file'] = str(archive_file)
+
+data['ydl_opts'] = { 
     'format': 'bestaudio',
     'cookiesfrombrowser': ('chrome', 'Profile 1'),
-    'download_archive': f'{data["archive_file"]}',
+    'download_archive': data["archive_file"],
+    # casting to Path object to get proper seperator, then recast to str for
+    # JSON storage
+    'outtmpl': str(Path(data["dl_folder"])) + '\\' + '%(title)s.%(ext)s',
     'postprocessors': [{  
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '320',
-        'outtmpl': f"{data['dl_folder'] + '%(title)s.%(ext)s'}"
         },  
         {
         'key': 'FFmpegMetadata',  
         }],
     
-        }
-with YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url_to_download])    
+    }
 
-
-
-# archive_file = fd.askopenfilename(title="Select Archive File")
-# proc_list = ['yt-dlp', '--cookies-from-browser', 'chrome:Profile 1', 
-#              '-x', '--audio-format', 'mp3', '--abort-on-error', 
-#              '--audio-quality', '0', playlist, '--download-archive', archive_file, '-o', dl_folder, '--exec',
-#              'stdout=STDOUT', 'check=True', 'text=True']
-
-# completed_proc = subprocess.run(proc_list)
-
-# ydl_opts = {'format': 'bestaudio'}
-# with YoutubeDL(ydl_opts) as ydl:
-#     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+with YoutubeDL(data['ydl_opts']) as ydl:
+    ydl.download([data['url_to_download']])
     
-# chosen_folder = ""
-# try:
-#      with open(dl_folder) as f:
-#        chosen_folder = f.readline()
-# except Exception as e:
-#      print(e)
-#      while (chosen_folder == "."):
-#           chosen_folder = str(Path(filedialog.askdirectory(title="Select Download Destination")))
-        
-# root = tk.Tk()
-# root.title("Greetings Human")
-# while (chosen_folder == ""):
-#      chosen_folder = str(Path(filedialog.askdirectory(title="Select Download Destination")))
-
-# subprocess.run(["yt-dlp", ])
-
-     
-# print(chosen_folder)
-# root.mainloop()
+with open(data_file, 'w') as f:
+    json.dump(data, f, indent=4, sort_keys=True)
+    
+print("All done!")
